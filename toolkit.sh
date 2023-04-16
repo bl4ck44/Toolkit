3a549bca325907fea676df0beea4d93eddde0980
bl4ck44 <hackingbl4ck44@gmail.com>
2023-04-16 14:57:41 -0600

    Version 1

--- toolkit.sh
+++ toolkit.sh
@@ -1,5 +1,67 @@
 #!/bin/bash
 
+# Ruta de la carpeta a comprobar
+ruta_carpeta="Herramientas/Hacking-Sistemas"
+
+# Comprobar si la carpeta existe
+if [ -d "$ruta_carpeta" ]; then
+  echo "\033[1m [+] La carpeta ya existe" > /dev/null
+  echo
+else
+  echo
+  # Si la carpeta no existe, se crea
+  mkdir "$ruta_carpeta"
+  echo "\033[1m [+] La carpeta ha sido creada" > /dev/null
+fi
+
+
+ruta_carpeta="Herramientas/Hacking-Web"
+
+if [ -d "$ruta_carpeta" ]; then
+  echo "\033[1m [+] La carpeta ya existe" > /dev/null
+  echo
+else
+  echo
+  mkdir "$ruta_carpeta"
+  echo "\033[1m [+] La carpeta ha sido creada" > /dev/null
+fi
+
+
+ruta_carpeta="Herramientas/Hacking-Wifi"
+
+if [ -d "$ruta_carpeta" ]; then
+  echo "\033[1m [+] La carpeta ya existe" > /dev/null 
+  echo
+else
+  echo
+  mkdir "$ruta_carpeta"
+  echo "\033[1m [+] La carpeta ha sido creada" > /dev/null
+fi
+
+
+ruta_carpeta="Herramientas/Ingenieria-Social"
+
+if [ -d "$ruta_carpeta" ]; then
+  echo "\033[1m [+] La carpeta ya existe" > /dev/null
+  echo
+else
+  echo
+  mkdir "$ruta_carpeta"
+  echo "\033[1m [+] La carpeta ha sido creada" > /dev/null
+fi
+
+
+ruta_carpeta="Herramientas/OSINT"
+
+if [ -d "$ruta_carpeta" ]; then
+  echo "\033[1m [+] La carpeta ya existe" > /dev/null
+  echo
+else
+  echo
+  mkdir "$ruta_carpeta"
+  echo "\033[1m [+] La carpeta ha sido creada" > /dev/null
+fi
+
 while :
 do
 
